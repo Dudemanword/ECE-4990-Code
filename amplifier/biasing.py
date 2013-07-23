8525c9db41afc6998956eba61d031df115c51425
The Greatest <shaunraj@gmail.com>
2013-07-23 15:07:44 -0700

    Cleaned up biasing code

--- amplifier/biasing.py
+++ amplifier/biasing.py
@@ -28,7 +28,7 @@ def r_pi(vt, ib):
 	rpi = vt/ib
 	return rpi
 
-def find_rs(r1,r2,rpi):
+def find_rp(r1,r2,rpi):
 	rin = r1*r2/(r1+r2) 
 	rin_real = rin*rpi/(rin + rpi)+10
 	print rin_real
@@ -63,17 +63,27 @@ def parallel(a, b):
 	return ans
 
 #Do calculations and stuff
-r_first = parallel(100, (1/(j*omega_0*C)))
-ro = parallel(r_first, 1.535e3)
-zs = find_zs(ro,100, omega_0, 5.134e-9, 1.592e-12)
-#rpi = r_pi(vt,ib)
-#rp = find_rs(r1,r2,rpi)
-rp = abs(ro)
-rs = 50
-q = calc_q(rp, rs)
-ls = find_ls(rs,q,omega_0)
-c = find_c(omega_0, ls)
+def right():
+	ro = parallel(parallel(100, (1/(j*omega_0*C))), 1.535e3)
+	zs = find_zs(ro,100, omega_0, 5.134e-9, 1.592e-12)
+	rs = 50
+	rp = abs(ro)
+	q = calc_q(rp, 50)
+	ls = find_ls(rs,q,omega_0)
+	c = find_c(omega_0, ls)
+	print 'RIGHT -----------------------------------------------------------------------------------------\n'
+	print 'rs is ' + str(rs), 'q is ' + str(q), 'rp is ' + str(rp), 'ls is ' + str(ls), 'c is ' + str(c), 'zs is ' + str(abs(zs))
+	print ' ---------------------------------------------------------------------------\n'
+def left():
+	rpi = r_pi(vt,ib)
+	rp = find_rp(r1,r2,rpi)
+	rs = 50
+	q = calc_q(rp, rs)
+	ls = find_ls(rs,q,omega_0)
+	c = find_c(omega_0, ls)
+	print 'LEFT -----------------------------------------------------------------------------------------\n'
+	print 'rs is ' + str(rs), 'q is ' + str(q), 'rp is ' + str(rp), 'ls is ' + str(ls), 'c is ' + str(c),'rpi is ' + str(rpi)
+	print ' ---------------------------------------------------------------------------\n'
 
-print 'rs is ' + str(rs), 'q is ' + str(q), 'rp is ' + str(rp), 'ls is ' + str(ls), 'c is ' + str(c), 'zs is ' + str(abs(zs))
-#print 'rs is ' + str(rs), 'q is ' + str(q), 'rp is ' + str(rp), 'ls is ' + str(ls), 'c is ' + str(c),'rpi is ' + str(rpi)
-#'rpi is ' + str(rpi), 
+right()
+left()
